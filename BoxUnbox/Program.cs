8f2402794e5f1b1bddaa5150de98a20d3c0302d1
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-19 11:55:09 -0300

    boxing e unboxing

---
+++ BoxUnbox/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BoxUnbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boxing!!");
+            int x = 20;
+            Object obj = x;
+            Console.WriteLine(obj);
+
+            Console.WriteLine("UnBoxing!!");
+            x = 20;
+            int y = (int) obj;
+
+        }
+    }
+}
